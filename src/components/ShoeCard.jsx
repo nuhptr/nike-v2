2133479d8aae783a130d0d425c6f681d736cc5c3
Adi Nugraha Putra <nugrahaadi733@gmail.com>
2023-08-19 22:49:25 +0700

    upidate ShoeCard component

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -1,3 +1,25 @@
-export default function ShoeCard() {
-   return <div>ShoeCard</div>;
+export default function ShoeCard({ shoes, changeBigShoeImg, bigShoeImg }) {
+   const handleClick = () => {
+      if (bigShoeImg !== shoes.bigShoe) {
+         changeBigShoeImg(shoes.bigShoe);
+      }
+   };
+
+   return (
+      <div
+         className={`border-2 rounded-xl ${
+            bigShoeImg === shoes.bigShoe ? 'border-coral-red' : 'border-transparent'
+         } cursor-pointer max-sm:flex-1`}
+         onClick={handleClick}>
+         <div className='flex justify-center items-center bg-card bg-center bg-cover sm:h-40 rounded-xl max-sm:p-4'>
+            <img
+               src={shoes.thumbnail}
+               alt='Image thumbnail'
+               width={150}
+               height={103}
+               className='object-contain'
+            />
+         </div>
+      </div>
+   );
 }
